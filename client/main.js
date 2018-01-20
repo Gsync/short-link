1d97dc3f557957c19e11196f2e0e78f1f9c76964
Khuram Niaz <knshaikh@gmail.com>
2018-01-20 16:44:58 -0700

    Basic routes with react-router

--- client/main.js
+++ client/main.js
@@ -2,13 +2,22 @@ import "babel-polyfill";
 import { Meteor } from "meteor/meteor";
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router, Route, browserHistory } from 'react-router';
 
-class Signup extends React.Component {
-  render() {
-    return <p>Signup componenet here</p>;
-  }
-}
+import Signup from '../imports/ui/Signup';
+import Login from '../imports/ui/Login';
+import Link from '../imports/ui/Link';
+import NotFound from '../imports/ui/NotFound';
+
+const routes = (
+  <Router history={browserHistory}>
+    <Route path="/signup" component={Signup} />
+    <Route path="/" component={Login} />
+    <Route path="/links" component={Link} />
+    <Route path="*" component={NotFound} />
+  </Router>
+);
 
 Meteor.startup(() => {
-  ReactDOM.render(<Signup />, document.getElementById('app'));
+  ReactDOM.render(routes, document.getElementById('app'));
 })
