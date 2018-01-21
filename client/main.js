9b4661f19c8e5c7cdbc371adeb79f3133691dae9
Khuram Niaz <knshaikh@gmail.com>
2018-01-20 20:37:19 -0700

    user authentication - control user access

--- client/main.js
+++ client/main.js
@@ -3,11 +3,15 @@ import { Meteor } from "meteor/meteor";
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router, Route, browserHistory } from 'react-router';
+import { Tracker } from 'meteor/tracker';
 
 import Signup from '../imports/ui/Signup';
 import Login from '../imports/ui/Login';
 import Link from '../imports/ui/Link';
 import NotFound from '../imports/ui/NotFound';
+
+const unauthenticatedPages = ['/', '/signup'];
+const authenticatedPages = ['/links'];
 
 window.browserHistory = browserHistory;
 
@@ -19,6 +23,21 @@ const routes = (
     <Route path="*" component={NotFound} />
   </Router>
 );
+
+Tracker.autorun(() => {
+  const isAuthenticated = !!Meteor.userId(); //is there is user, return true
+  const pathname = browserHistory.getCurrentLocation().pathname;
+  const isUnauthenticatedPage = unauthenticatedPages.includes(pathname);
+  const isAuthenticatedPage = authenticatedPages.includes(pathname);
+
+  if (isUnauthenticatedPage && isAuthenticated) {
+    browserHistory.push('/links');
+  }
+  else if (isAuthenticatedPage && !isAuthenticated) {
+    browserHistory.push('/');
+  }
+
+});
 
 Meteor.startup(() => {
   ReactDOM.render(routes, document.getElementById('app'));
