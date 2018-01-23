3a9a3ef00002b397778b5b77163fa3d795f2945b
Khuram Niaz <knshaikh@gmail.com>
2018-01-23 16:15:03 -0700

    refactor routes

--- server/main.js
+++ server/main.js
@@ -1,48 +1,9 @@
 import { Meteor } from 'meteor/meteor';
 import "babel-polyfill";
-import SimpleSchema from 'simpl-schema';
-import { Accounts } from 'meteor/accounts-base';
+
+import '../imports/api/users';
 
 Meteor.startup(() => {
   // code to run on server at startup
-
-  Accounts.validateNewUser((user) => {
-    const email = user.emails[0].address;
-
-    try {
-      new SimpleSchema({
-        email: {
-          type: String,
-          regEx: SimpleSchema.RegEx.Email
-        }
-      }).validate({ email });
-    } catch (e) {
-      throw new Meteor.Error(400, e.message);
-    }
-    return true;
-  });
-
-  // const employeeSchema = new SimpleSchema({
-  //   name: {
-  //     type: String,
-  //     min: 1,
-  //     max: 100
-  //   },
-  //   hourlyWage: {
-  //     type: Number,
-  //     min: 0
-  //   },
-  //   email: {
-  //     type: String,
-  //     regEx: SimpleSchema.RegEx.Email
-  //   }
-  // });
-
-
-  // employeeSchema.validate({
-  //   name: 'Jack',
-  //   hourlyWage: 14,
-  //   email: 's1234@test.com'
-  // });
 
 });
